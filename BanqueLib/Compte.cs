fce3675630a1ce0fa99fe830b638181829dd1846
Tristan086 <6292338@cstjean.qc.ca>
2025-03-17 12:18:44 -0400

    Commit Final

---
+++ BanqueLib/Compte.cs
@@ -0,0 +1,123 @@
+﻿namespace BanqueLib
+{
+    //éléments requis à la classe
+    public enum Statut {OK, Gelé, Vide}
+    //classe
+    public class Compte
+    {
+        #region ---- Attributs ----
+        private readonly int _numero;
+        private string _detenteur;
+        private decimal _solde;
+        private bool _estGele = false;
+        private Statut _statut;
+        #endregion
+        #region ---- Constructeur ----
+        public Compte(int numero, string detenteur, decimal solde = 0m, Statut statut = Statut.OK, bool estGele = false)
+        {
+            _numero = numero;
+            SetDetenteur(detenteur);
+            decimal.Round(solde, 2);
+            _solde = solde;
+            _estGele = estGele;
+            _statut = statut;
+        }
+        #endregion
+        #region ---- Getters ----
+        public int Numero => _numero;
+        public string Detenteur => _detenteur;
+        public decimal Solde => _solde;
+        public bool EstGele => _estGele;
+        public Statut Statut => _statut;
+        #endregion
+        #region ---- Setters ----
+        public void SetDetenteur(string detenteur) {
+            if (string.IsNullOrWhiteSpace(detenteur))
+                throw new ArgumentNullException();
+            detenteur = detenteur.Trim();
+            _detenteur = detenteur;
+        }
+        #endregion
+        #region ---- Méthodes ----
+        public string Description() {
+            string description = "";
+            description += $"[TG] *******************************************\n";
+            description += $"[TG] *                                         *\n";
+            description += $"[TG] *    COMPTE {_numero, -30}*\n";
+            description += $"[TG] *       De: {_detenteur, -30}*\n";
+            description += $"[TG] *    Solde: {_solde, -30:C}*\n";
+            description += $"[TG] *   Statut: {_statut, -30}*\n";
+            description += $"[TG] *                                         *\n";
+            description += $"[TG] *******************************************\n";
+            return description;
+        }
+        public bool PeutDeposer(decimal montant = 1) {
+            var arrondi = decimal.Round(montant, 2);
+            if (montant <= 0 || montant != arrondi)
+                throw new ArgumentOutOfRangeException();
+            if (!_estGele)
+                return true;
+            else
+                return false;
+        }
+        public bool PeutRetirer(decimal montant = 1)
+        {
+            var arrondi = decimal.Round(montant, 2);
+            if (montant <= 0 || montant != arrondi)
+                throw new ArgumentOutOfRangeException();
+            if (!_estGele)
+                return true;
+            else
+                return false;
+        }
+
+        public decimal Deposer(decimal montant) {
+            var arrondi = decimal.Round(montant, 2);
+            if (montant <= 0 || montant != arrondi)
+                throw new ArgumentOutOfRangeException();
+            if (PeutDeposer(montant))
+            {
+                _solde += montant;
+                return _solde;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public decimal Retirer(decimal montant)
+        {
+            var arrondi = decimal.Round(montant, 2);
+            if (montant <= 0 || montant != arrondi)
+                throw new ArgumentOutOfRangeException();
+            if (PeutRetirer(montant))
+            {
+                _solde -= montant;
+                return _solde;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public decimal Vider() {
+            if (_statut == Statut.Vide || _statut == Statut.Gelé)
+                throw new InvalidOperationException();
+            else
+            {
+                decimal montant = _solde;
+                _solde -= montant;
+                _statut = Statut.Vide;
+                return montant;
+            }
+        }
+        public void Geler()
+        {
+            if (_statut == Statut.Gelé)
+                throw new InvalidOperationException();
+            else
+                _statut = Statut.Gelé;
+        }
+        public void Degeler()
+        {
+            _statut = Statut.OK;
+        }
+        #endregion
+    }
+}
---
+++ ConsoleTesterCompte/ConsoleTesterCompte.cs
@@ -0,0 +1,200 @@
+﻿using BanqueLib;
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("""
+    
+     TESTER COMPTE
+
+     1 - Création d'un compte simple (solde 0$)
+     2 - Création d'un compte avec solde
+     3 - Création d'un compte gelé
+     4 - Déposer
+     5 - Retirer
+     6 - Vider
+     7 - Geler et dégeler
+     8 - Getters
+     9 - Setter
+     a - Exceptions (constructeur)
+     b - Exceptions (setter)
+     c - Exceptions (peutdéposer et peutretirer)
+     d - Exceptions (déposer et retirer)
+     e - Exceptions (geler, dégeler et vider)
+     q - Quitter
+
+     Votre choix, Tristan Gordon ?
+
+    """);
+
+    switch (Console.ReadKey(true).KeyChar)
+    {
+        case '1':
+            Console.WriteLine(new Compte(1001, "Mme Création simple").Description());
+            break;
+        case '2':
+            Console.WriteLine(new Compte(1002, "         Mme Création avec solde        ", 8008.02m).Description());
+            break;
+        case '3':
+            Console.WriteLine(new Compte(1003, "Mme Création gelée", 1_111_111, Statut.Gelé).Description());
+            break;
+        case '4': // Déposer
+            {
+                var compte = new Compte(1004, "M Déposer", 1000);
+                Console.WriteLine(" " + compte.PeutDeposer());
+                Console.WriteLine(" " + compte.PeutDeposer(1000));
+                Console.WriteLine();
+                compte.Deposer(1000);
+                Console.WriteLine(compte.Description());
+            } 
+            break;
+        case '5': // Retirer
+            {
+                var compte = new Compte(1005, "M Retirer", 5000);
+                Console.WriteLine(" " + compte.PeutRetirer());
+                Console.WriteLine(" " + compte.PeutRetirer(1000));
+                Console.WriteLine(" " + !compte.PeutRetirer(10000));
+                Console.WriteLine();
+                compte.Retirer(1000);
+                Console.WriteLine(compte.Description());
+            }
+            break;
+        case '6': // Vider
+            {
+                var compte = new Compte(1006, "M Vider", 15000);
+                Console.WriteLine(" " + (compte.Vider() == 15000));
+                Console.WriteLine("\n" + compte.Description());
+            }
+            break;
+        case '7': // Geler et dégeler
+            {
+                var compte = new Compte(1007, "MM Gel et Dégel", 7000);
+                compte.Geler();
+                Console.WriteLine(" " + !compte.PeutDeposer());
+                Console.WriteLine(" " + !compte.PeutRetirer());
+                Console.WriteLine("\n" + compte.Description());
+                compte.Degeler();
+                Console.WriteLine("\n " + compte.PeutDeposer());
+                Console.WriteLine(" " + compte.PeutRetirer());
+                Console.WriteLine("\n" + compte.Description());
+            }
+            break;
+        case '8': // getters
+            {
+                var compte = new Compte(1008, "Mme getters", 8000);
+                Console.WriteLine(compte.Description());
+                Console.WriteLine();
+                Console.WriteLine($"          Numéro: {compte.Numero}");
+                Console.WriteLine($" Détenteur.trice: {compte.Detenteur}");
+                Console.WriteLine($"           Solde: {compte.Solde:C}");
+                Console.WriteLine($"          Statut: {compte.Statut}");
+                Console.WriteLine($"            Gelé: {compte.EstGele}\n");
+            }
+            break;
+        case '9': // setters
+            {
+                var compte = new Compte(1008, "Mme setter", 9000);
+                compte.SetDetenteur("          Mme setter anglais        ");
+                Console.WriteLine($" {compte.Detenteur == "Mme setter anglais"}\n");
+                Console.WriteLine(compte.Description());
+            }
+            break;
+        case 'a':  // erreurs de constructions
+            {
+                (string, string?)[] erreurs = 
+                {
+                    Utile.ExceptMsg(() => new Compte(0, "Han")),
+                    Utile.ExceptMsg(() => new Compte(-1, "Han")),
+                    Utile.ExceptMsg(() => new Compte(1, null!)),
+                    Utile.ExceptMsg(() => new Compte(1, "")),
+                    Utile.ExceptMsg(() => new Compte(1, "    ")),
+                    Utile.ExceptMsg(() => new Compte(1, "Han", -1)),
+                    Utile.ExceptMsg(() => new Compte(1, "Han", 0.001m)),
+                };
+                foreach (var (excep, message) in erreurs)
+                    Console.WriteLine($"\n {excep}\n {message ?? ""}");
+            }
+            break;
+        case 'b':  // erreurs de setter
+            {
+                var ok = new Compte(1009, "Obiwan", 7000);
+                (string, string?)[] erreurs =
+                {
+                    Utile.ExceptMsg(() => ok.SetDetenteur(null!)),
+                    Utile.ExceptMsg(() => ok.SetDetenteur("")),
+                    Utile.ExceptMsg(() => ok.SetDetenteur("  ")),
+                };
+                foreach (var (excep, message) in erreurs)
+                    Console.WriteLine($"\n {excep}\n {message ?? ""}");
+            }
+            break;
+        case 'c':  // erreurs PeutRetirer, PeutDéposer
+            {
+                var ok = new Compte(1009, "Obiwan", 7000);
+                (string, string?)[] erreurs =
+                {
+                    Utile.ExceptMsg(() => ok.PeutDeposer(-1)),
+                    Utile.ExceptMsg(() => ok.PeutDeposer(1.001m)),
+                    Utile.ExceptMsg(() => ok.PeutRetirer(-1)),
+                    Utile.ExceptMsg(() => ok.PeutRetirer(1.001m)),
+                };
+                foreach (var (excep, message) in erreurs)
+                    Console.WriteLine($"\n {excep}\n {message ?? ""}");
+            }
+            break;
+        case 'd':  // erreurs de retrait et dépôt
+            {
+                var ok = new Compte(1009, "Obiwan", 7000);
+                var gelé = new Compte(1009, "Obiwan", 7000, Statut.Gelé);
+                (string, string?)[] erreurs =
+                {
+                    Utile.ExceptMsg(() => ok.Deposer(1000.001m)),
+                    Utile.ExceptMsg(() => gelé.Deposer(1000)),
+                    Utile.ExceptMsg(() => ok.Retirer(8000)),
+                    Utile.ExceptMsg(() => ok.Retirer(1000.001m)),
+                    Utile.ExceptMsg(() => gelé.Retirer(1000)),
+                };
+                foreach (var (excep, message) in erreurs)
+                    Console.WriteLine($"\n {excep}\n {message ?? ""}");
+            }
+            break;
+        case 'e':  // erreurs pour geler, dégeler et vider
+            {
+                var ok = new Compte(1009, "Obiwan", 0);
+                var gelé = new Compte(1009, "Obiwan", 10, Statut.Gelé);
+                (string, string?)[] erreurs =
+                {
+                    Utile.ExceptMsg(() => ok.Degeler()),
+                    Utile.ExceptMsg(() => gelé.Geler()),
+                    Utile.ExceptMsg(() => ok.Vider()),
+                    Utile.ExceptMsg(() => gelé.Vider()),
+                };
+                foreach (var (excep, message) in erreurs)
+                    Console.WriteLine($"\n {excep}\n {message ?? ""}");
+            }
+            break;
+        case 'q':
+            Environment.Exit(0); break;
+        default:
+            Console.WriteLine(" Mauvais choix"); break;
+    }
+    Console.WriteLine("\n Appuyer sur ENTER pour continuer...");
+    Console.ReadLine();
+}
+
+#pragma warning disable S3903 // Types should be defined in named namespaces
+static class Utile
+{
+    public static (string, string?) ExceptMsg(Action action)
+    {
+        try
+        {
+            action();
+            return ("EXCEPTION attendue", null);
+        }
+        catch (Exception ex)
+        {
+            return (ex.GetType().Name, ex.Message);
+        }
+    }
+}
